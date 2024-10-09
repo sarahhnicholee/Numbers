d6c522db5bfef852316e3e1709825322d428c9ec
Sarah Parent <sarahparent@Sarahs-MacBook-Air.local>
2024-10-08 23:34:04 -0500

    hwcode

--- index.js
+++ index.js
@@ -10,27 +10,43 @@ class Numbers {
     }
   }
   count() {
+    return this.data.length
     //return the count of numbers in data
   }
   printNumbers() {
+    for (let i=0; i<this.data.length; i++){
+      console.log(this.data[i])
+    }
     //print the numbers in data
   }
   odds() {
+    let oddNums= this.data.filter((number) => number % 2 !==0) 
     //return the odd numbers in data
+    return oddNums
   }
   evens() {
+    let evenNums= this.data.filter((number)=> number % 2 ===0)
+    return evenNums
     //return the even numbers in data
   }
   sum() {
+    let sumOfArr= this.data.reduce((acc,number)=> acc+number,0)
     //return the sum of the numbers
-  }
+  return sumOfArr
+}
   product() {
+    let prodOfArr= this.data.reduce((acc,number)=> acc* number,1)
+   return prodOfArr
     //return the product of the numbers
   }
   greaterThan(target) {
+    let greatThanThree= this.data.filter((number)=> number> target)
+    return greatThanThree
     //return the numbers greater than the target
   }
   howMany(target) {
+    let howMan= this.data.filter((number)=> number=== target).length
+    return howMan
     //return the count of a given number
   }
 }
@@ -40,7 +56,7 @@ const str = prompt("enter some numbers, like this", "1,2,3,3,5,9");
 
 //create an instance of numbers
 const n1 = new Numbers(str);
-console.log(n1.count()); //returns count of numbers
+console.log(n1.count()) //returns count of numbers
 n1.printNumbers(); //prints the number along with their indexes
 console.log(n1.odds()); //returns odd numbers
 console.log(n1.evens()); //returns even numbers
